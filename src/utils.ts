9926ce8536c3f3364fbc840320a1a26be4ff4f02
ferriprasetya <irrefaytesarp@gmail.com>
2023-03-19 18:47:54 +0700

    delete unused code

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,6 @@ export const formatLongNumber = (num: number) => {
   let units = ['k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
   let floor = Math.floor(Math.abs(num).toString().length / 3);
   let value=+(num / Math.pow(1000, floor))
-  console.log("floor", floor);
   return (+(value).toFixed(2)).toLocaleString("id") + (floor ? units[floor - 1] : '');
 };
 
